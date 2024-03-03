d0597c56c021ba9623f2fcdcb89eb61426cb2ab3
Shakar Kosar <shakarkosar01@gmail.com>
2024-03-03 12:58:19 +0300

    ref: nuxt config file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
+	pages: true,
 	srcDir: 'src/',
-	modules: ['@nuxtjs/eslint-module'],
+	modules: [
+		'nuxt-icon',
+		'nuxt-lodash',
+		'@pinia/nuxt',
+		'@pinia-plugin-persistedstate/nuxt',
+		'@nuxtjs/eslint-module',
+		'@nuxtjs/tailwindcss',
+		'@nuxtjs/supabase'
+	],
+	runtimeConfig: {
+		public: {
+			stripePk: process.env.STRIPE_PK_KEY
+		}
+	},
+	app: {
+		head: {
+			script: [{ src: 'https://js.stripe.com/v3/', defer: true }]
+		}
+	},
 	css: ['@/assets/styles/main.scss'],
 	postcss: {
 		plugins: {
