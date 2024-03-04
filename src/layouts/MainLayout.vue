77d60249b9f31008ef98f9acf5167d9f97dab35c
sShakar <shakarkosar01@gmail.com>
2024-03-04 22:24:57 +0300

    push

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,5 +1,10 @@
 <template>
-	<main id="MainLayout" class="bg-amber-100">
+	<main id="MainLayout" class="fixed z-50 w-full">
+		<div id="TopMenu" class="hidden w-full border-b bg-[#FAFAFA] md:block">
+			<ul
+				class="flex h-10 max-w-[1200px] items-center justify-end bg-[#FAFAFA] px-2 text-xs font-light text-[#333333]"
+			></ul>
+		</div>
 		<slot />
 	</main>
 </template>
